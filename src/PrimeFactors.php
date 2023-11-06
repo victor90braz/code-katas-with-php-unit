5a0447cc8e13445b2fb316c3fedbb6a48d81fdfc
victor braz <victor.90braz@gmail.com>
2023-11-06 11:14:16 +0100

    tdd

--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -6,10 +6,20 @@ class PrimeFactors {
 
   public function generate($number)
   {
-    if ($number > 1) {
-      return [$number];
+    $factors = [];
+    $divisor = 2;
+
+    while($number > 1) {
+
+      while ($number %  $divisor === 0) {
+        $factors[] =  $divisor;
+
+        $number = $number / $divisor;
+      }
+
+      $divisor++;
     }
 
-    return [];
+    return $factors;
   }
 }
--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -22,4 +22,22 @@ class PrimeFactorsTest extends TestCase {
 
     $this->assertEquals([2], $factors->generate(2));
   }
+
+  /** @test */
+  function it_generates_prime_factors_for_3()
+  {
+
+    $factors = new PrimeFactors();
+
+    $this->assertEquals([3], $factors->generate(3));
+  }
+
+  /** @test */
+  function it_generates_prime_factors_for_4()
+  {
+
+    $factors = new PrimeFactors();
+
+    $this->assertEquals([2,2], $factors->generate(4));
+  }
 }
