44a2c82cbf6179988685d5618e0c1e41adac769e
victor braz <victor.90braz@gmail.com>
2023-11-06 13:24:09 +0100

    static::NUMERALS as $numeral => $arabic

--- src/RomanNumerals.php
+++ src/RomanNumerals.php
@@ -4,14 +4,31 @@ namespace App;
 
 class RomanNumerals {
 
+  const NUMERALS = [
+    'M'  => 1000,
+    'CM' => 900,
+    'D'  => 500,
+    'CD' => 400,
+    'C'  => 100,
+    'XC' => 90,
+    'L'  => 50,
+    'XL' => 40,
+    'X'  => 10,
+    'IX' => 9,
+    'V'  => 5,
+    'IV' => 4,
+    'I'  => 1,
+  ];
+
   public static function generate($number)
   {
     $result = '';
 
-    while ($number > 0) {
-      $result .= 'I'; // Use the '.' operator for string concatenation
-
-      $number -= 1;
+    foreach(static::NUMERALS as $numeral => $arabic) {
+      while ($number >= $arabic) {
+        $result .= $numeral;
+        $number -= $arabic;
+      }
     }
 
     return $result;
