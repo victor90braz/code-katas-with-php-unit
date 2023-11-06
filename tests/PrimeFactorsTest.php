119d9033ae6493f9498798c43018aaca0d45ee7d
victor braz <victor.90braz@gmail.com>
2023-11-06 11:23:22 +0100

    it_generates_prime_factors_for_numbers

--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -9,39 +9,12 @@ class PrimeFactorsTest extends TestCase {
    * @test
    * @dataProvider factors
    * */
-  function it_generates_prime_factors_for_1($number, $expected)
+  function it_generates_prime_factors_for_numbers($number, $expected)
   {
 
     $factors = new PrimeFactors();
 
     $this->assertEquals($expected, $factors->generate($number));
-  }
-
-  /** @test */
-  function it_generates_prime_factors_for_2()
-  {
-
-    $factors = new PrimeFactors();
-
-    $this->assertEquals([2], $factors->generate(2));
-  }
-
-  /** @test */
-  function it_generates_prime_factors_for_3()
-  {
-
-    $factors = new PrimeFactors();
-
-    $this->assertEquals([3], $factors->generate(3));
-  }
-
-  /** @test */
-  function it_generates_prime_factors_for_4()
-  {
-
-    $factors = new PrimeFactors();
-
-    $this->assertEquals([2,2], $factors->generate(4));
   }
 
   public static function factors()
